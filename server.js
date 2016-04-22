8eb0522fe2ca2812b68d8029505cc54bda5b4223
Ulrik Augustsson <ulrikaugustsson@gmail.com>
2016-04-22 16:35:45 +0200

    Cleanup and add license

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
 const express = require('express');
-const httpProxy = require('http-proxy');
 
 const app = express();
-const proxy = httpProxy.createProxyServer({});
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
-
-app.use('/image', (req, res) => {
-  console.log(req.query);
-  proxy.web(req, res, { target: req.query.url }, function (err) {
-    console.log(err);
-  });
-});
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`)
